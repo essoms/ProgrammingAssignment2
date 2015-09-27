8a397d4dc224e3886125506b23d0752c601623bc
Essomba Ghislain <thaddee.ghislain@gmail.com>
2015-09-27 20:56:32 +0100

    Add cute cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,24 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special matrix, which is really a list containing a function to
+##set the values of the matrix
+##get the values of the matrix
+##set the inverse of the matrix 
+##get the inverse of the matrix 
 makeCacheMatrix <- function(x = matrix()) {
+	  i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) i <<- inverse
+        getInverse <- function() i
+        list(set = set,
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 }
 
@@ -12,4 +28,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        mat <- x$get()
+        i <- solve(mat, ...)
+        x$setInverse(i)
+        i
 }
